b5f3503f3cf798a78c6d54e29e444ce08349c4b9
unknown <danielaballesteros85@gmail.com>
2020-02-20 22:27:37 -0300

    Animación sección sobre mi, sección experiencias-Cards

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+
+
+    $(document).scroll(function () {
+        var component = $("#sobre-mi").offset().top;
+        var altura = $(document).scrollTop();
+        if (Math.round(altura) > (Math.round(component) - 200)) {
+            $("#foto").animate({ left: '0' });
+            $(".parrafo").slideDown();
+        }
+
+    })
+
+    $(".sobre-mi-link").click(function () {
+        $("#foto").animate({ left: '0' });
+    });
+
+    $(".sobre-mi-link").click(function () {
+        $(".parrafo").slideDown();
+    });
+});
+
+    // $('.sobre-mi-link').click(function (event) {
+    //     event.preventDefault(); // because it is an anchor element
+    //     $('#parrafo').animate({
+    //         right: '200px'
+    //     });
+    //     //$('#slideMenu').toggle();
+    // });
